b35ba0c2b233072536f2c297e51956cfc04909cc
r-ume <baymax.blackandwhite@gmail.com>
2017-04-30 22:19:04 +0900

    solved problem-009

---
+++ project_euler/problem-009.rb
@@ -0,0 +1,18 @@
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+def pythagorean_triplet?(a, b, c)
+  if c ** 2 == (a ** 2 + b ** 2)
+    return true
+  end
+  false
+end
+
+(1..1000).each do |a|
+  (a..1000).each do |b|
+    c = 1000-a-b
+    if pythagorean_triplet?(a, b, c)
+      puts "Answer: #{a*b*c}"
+      break
+    end
+  end
+end
