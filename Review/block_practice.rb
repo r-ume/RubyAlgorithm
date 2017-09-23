282a74be1077f95a1b9c7da1fb45739896c4f5f3
r-ume <baymax.blackandwhite@gmail.com>
2017-09-23 16:10:13 +0900

    added block_practice.rb

---
+++ Review/block_practice.rb
@@ -0,0 +1,84 @@
+def block_sample
+  puts 'stand up'
+  # yieldの実装を呼びだし先で定義する。
+  yield if block_given?
+  puts 'sit down'
+end
+
+block_sample do
+  p 'walk'  
+end
+
+# File Object without Block
+file = File.open('without_block.txt', 'w')
+file.puts 'without block'
+file.close
+
+# File Object with Block
+File.open 'with_block.txt', 'w' do |file|
+  file.puts 'with block'
+end
+
+def blocked_arg
+  yield()
+end
+
+blocked_arg do |a|
+  p [a]
+end
+
+# => nil
+
+def blocked_arg
+  yield(1, 2, 3)
+end
+
+blocked_arg do |a|
+  p a 
+end
+
+# => 1
+
+def block_arg
+  yield(1, 2, 3)
+end
+
+blocked_arg do | *a |
+  p a
+end
+
+# => p[1, 2, 3]
+
+def with_current_time
+  yield(Time.now)
+end
+
+with_current_time do |now|
+  puts now.year
+end
+
+# & 引数名にすると、与えられたブロックは、Procオブジェクト化される。
+def total(from, to, &block)
+  result = 0
+  from.upto(to) do |num|
+    if block
+      result += block.call(num)
+    else
+      result += num
+    end
+  end
+  result
+end
+
+p total(1, 10)
+p total(1, 10){ |num| num ** 2 }
+
+
+
+
+
+
+
+
+
+
