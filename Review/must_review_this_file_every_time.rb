92ab0f9936c6c8cf27619d78bb962fa4759e2261
r-ume <baymax.blackandwhite@gmail.com>
2017-09-24 20:37:22 +0900

    added more refactoring examples in must_review_this_file_every_time.rb

--- Review/must_review_this_file_every_time.rb
+++ Review/must_review_this_file_every_time.rb
@@ -48,3 +48,86 @@ end
 # After 
 Array(numbers).each{ |num| num.is_a?(Integer) }
 
+# Before 
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+# After
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  # No Need
+  # def name
+  #   @name
+  # end
+end
+
+# before
+def count_admin(users)
+  count = 0
+  users.each do |user|
+    count += 1 if user.admin?
+  end
+  count
+end
+
+# after
+def count_admin(users)
+  users.count(&:admin?)
+end
+
+# before
+def user_names(users)
+  names = []
+  users.each do |user|
+    names << user.name
+  end
+  names
+end
+
+# after
+def user_names(users)
+  users.map(&:name)
+end
+
+numbers = [1, 2, 3]
+
+# Before
+def numbers_text(numbers)
+  text = ''
+  numbers.each_with_index do |number, i|
+    text += ', ' if i > 0
+    text += number.to_s
+  end
+  text
+end
+
+# After
+def numbers_text(numbers)
+  numbers.join(',')
+end
+
+# Before
+def oldest_user(users)
+  oldest = nil
+  users.each  do |user|
+    oldest = user if oldest.nil? || user.age > oldest.age
+  end
+  oldest
+end
+
+# After
+def oldest_user(users)
+  users.max_by(&:age)
+end
