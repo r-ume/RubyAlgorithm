1b2da4aa81d473a9f33d403ac82ca898014dd09a
r-ume <baymax.blackandwhite@gmail.com>
2017-10-19 10:37:54 +0900

    added single_responsibility file

--- Review/struct_practice.rb
+++ Review/struct_practice.rb
@@ -36,3 +36,58 @@ ThirdHuman.new(10).teen?
 ThirdHuman.new(14).teen?
 # human = Human.new(10)
 # p human.teen?
+
+class RevealingReferences
+  attr_reader :wheels
+
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.map { |wheel| diameter(wheel) }
+  end
+
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2 )
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.map { |cell| Wheel.new(cell[0], cell[1]) }
+  end
+end
+
+data = [[622, 20], [622, 23], [559, 30], [559, 40]]
+instance = RevealingReferences.new(data)
+p instance.diameters
+
+class Gear 
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog       = cog
+    @wheel     = Wheel.new(rim, tire)
+  end 
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+     
+  Wheel = Struct.new(:rim, :tire) do 
+    def diameter
+      rim + (tire * 2)
+    end
+  end
+end
+
+gear = Gear.new(1, 1, 1, 1)
+p gear.gear_inches
+# p gear.diameter => undefined
+
+
