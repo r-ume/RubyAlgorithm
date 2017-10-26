e9a187db78b921d90f5e8803979979f7c1c61d98
r-ume <baymax.blackandwhite@gmail.com>
2017-10-26 11:38:33 +0900

    had main_script.rb extract elements from enum and shifts in common

--- notification_on_slack/main_script.rb
+++ notification_on_slack/main_script.rb
@@ -6,9 +6,21 @@ require './slack_notifier.rb'
 require './mentors.rb'
 
 google_calendar = GoogleCalendar.new
-notifications = google_calendar.outputs_today_mentors_shift
+shifts = google_calendar.right_time_format_shifts
 
-p notifications = notifications.join(', ')
+shifts_with_mentions = []
 
-slack_notifier = SlackNotifier.new
-slack_notifier.sends_notification(notifications)
+Mentors.values.each { |value|
+  shifts.each { |shift|
+    if shift[:calendar_name].include?(value[:calendar_name])
+      shift_with_mention = value.merge(shift)
+      shifts_with_mentions << shift_with_mention
+    end
+  }
+}
+
+shifts_with_mentions.map do |shift_with_mention|
+  slack_notifier      = SlackNotifier.new
+  shifts_with_mention = "<#{shift_with_mention[:mention]}> : #{shift_with_mention[:calendar_name]} : #{shift_with_mention[:start_time]}"
+  slack_notifier.sends_notification(shifts_with_mention)
+end
