e5b0bdca662ad4e58a66926a863866b11aaabdc6
r-ume <baymax.blackandwhite@gmail.com>
2017-11-11 15:29:07 +0900

    defined get_images_response.rb

--- instagram_photos_slack_upload_integration/api_handler.rb
+++ instagram_photos_slack_upload_integration/api_handler.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'pry'
 require 'faraday'
 require 'dotenv'
+require 'JSON'
 
 Dotenv.overload
 
@@ -9,24 +10,18 @@ class ApiHandler
 
   USER_ID      = ENV['USER_ID']
   ACCESS_TOKEN = ENV['ACCESS_TOKEN']
-  API_DOMAIN   = 'https://api.instagram.com/v1/users/'
+  API_DOMAIN   = 'https://api.instagram.com/v1/users'
 
   class << self
-    def calls_image_api
-      res = Faraday.get image_get_url
-      p res.status    # => 200
-      p res.success?  # => true
-      res.headers.each do |k, v|
-        puts "#{k}: #{v}"
-      end
-      puts res.body
+    def gets_image_json_response
+      res = Faraday.get image_api
+      JSON.parse(res.body)
     end
 
-    def image_get_url
-      "https://api.instagram.com/v1/users/#{USER_ID}/media/recent/?access_token=#{ACCESS_TOKEN}"
+    def image_api
+      "#{API_DOMAIN}/#{USER_ID}/media/recent/?access_token=#{ACCESS_TOKEN}"
     end
   end
-
 
 end
 
