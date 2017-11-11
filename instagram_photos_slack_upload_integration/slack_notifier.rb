62a891deb68e7c3477657aaab52be07995c1e947
r-ume <baymax.blackandwhite@gmail.com>
2017-11-11 14:48:54 +0900

    added slack_notifier.rb file:

---
+++ instagram_photos_slack_upload_integration/slack_notifier.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'pry'
+require 'slack-notifier'
+
+class SlackNotifier
+
+  def initialize
+    @notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
+  end
+
+  def sends_notification(notification)
+    @notifier.ping(notification)
+  end
+
+  def sends_image(image_url)
+    @notifier.post(text: image_url)
+  end
+
+end
