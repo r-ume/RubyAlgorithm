55860ff83efbfac4253330589559c6d9a9ac178d
r-ume <baymax.blackandwhite@gmail.com>
2017-11-19 19:28:39 +0900

    added birthday to mentors enum

--- notification_on_slack/mentors.rb
+++ notification_on_slack/mentors.rb
@@ -5,16 +5,16 @@ require 'ruby-enum'
 class Mentors
   include Ruby::Enum
 
-  define 'JIO',     { mention: '@jiosotoyama',    calendar_name: 'じお' }
-  define 'YUTA',    { mention: '@yuta-takahashi', calendar_name: 'ゆうた' }
-  define 'YUKKI',   { mention: '@ueda-yukihiro',  calendar_name: 'ゆっきー' }
-  define 'UMEKI',   { mention: '@ryo-umeki',      calendar_name: 'うめきち' }
-  define 'SATSUKI', { mention: '@satsuki-kajiya', calendar_name: 'さつき' }
-  define 'MAKIY',   { mention: '@makiko-shimizu', calendar_name: 'まっきー' }
-  define 'SAWA',    { mention: '@sawa-uchida',    calendar_name: 'さわっち' }
-  define 'AWA',     { mention: '@keisuke.awa',    calendar_name: 'あわちゃん' }
-  define 'ANJU',    { mention: '@anju',           calendar_name: 'あんじゅ' }
-  define 'MAI',     { mention: '@takana-mai',     calendar_name: 'まい' }
+  define 'JIO',     { mention: '@jiosotoyama',    calendar_name: 'じお',      birthday: '05/29' }
+  define 'YUTA',    { mention: '@yuta-takahashi', calendar_name: 'ゆーた',     birthday: '08/19'}
+  define 'YUKKI',   { mention: '@ueda-yukihiro',  calendar_name: 'ゆっきー',   birthday: '11/05' }
+  define 'UMEKI',   { mention: '@ryo-umeki',      calendar_name: 'うめきち',   birthday: '10/15' }
+  define 'SATSUKI', { mention: '@satsuki-kajiya', calendar_name: 'さつき',    birthday:  '05/21' }
+  define 'MAKIY',   { mention: '@makiko-shimizu', calendar_name: 'まっきー',   birthday: '01/13'}
+  define 'SAWA',    { mention: '@sawa-uchida',    calendar_name: 'さわっち',   birthday: '10/12'}
+  define 'AWA',     { mention: '@keisuke.awa',    calendar_name: 'あわちゃん', birthday: '10/27' }
+  define 'ANJU',    { mention: '@anju',           calendar_name: 'あんじゅ',   birthday: '01/13' }
+  define 'MAI',     { mention: '@takana-mai',     calendar_name: 'まい',      birthday: '09/20'}
 
   def self.add_their_mentions_into(shifts)
     shifts_with_mentions = []
