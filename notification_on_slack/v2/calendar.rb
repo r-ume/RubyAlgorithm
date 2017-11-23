9991c6c092324a8d7ab00c42e0424fe006d863a6
r-ume <baymax.blackandwhite@gmail.com>
2017-11-24 01:15:24 +0900

    added calendar.rb

---
+++ notification_on_slack/v2/calendar.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'dotenv'
+require 'pry'
+require './util/google_authentication'
+require '../v2/calendar_item'
+
+# Utility (static class) for retrieving data from Google calendar.
+class Calendar
+  @@calendar = GoogleAuthentication.access_to_calendar_service
+
+  # Get items
+  # @return GoogleCalendarItem[]
+  class << self
+    def shifts_tomorrow
+      tomorrow_shifts = ids.map { |calendar_id|
+        @@calendar.list_events(calendar_id).items.select { |event|
+          CalendarItem.new( { calendar_name: event.summary, start_time: event.start.date_time } ).tomorrow_shift?
+        }
+      }
+      tomorrow_shifts.flatten
+    end
+
+    private
+    def ids
+      @@calendar.list_calendar_lists.items.reject{ |calendar| calendar.id == ENV['GMAIL_ACCOUNT'] }.map(&:id)
+    end
+  end
+
+end
