cda757f65829399c26903584157740fbe81dd6df
r-ume <baymax.blackandwhite@gmail.com>
2017-11-29 17:43:27 +0900

    declared class instance variable

--- notification_on_slack/v2/util/slack_notifier.rb
+++ notification_on_slack/v2/util/slack_notifier.rb
@@ -4,12 +4,13 @@ require 'slack-notifier'
 class SlackNotifier
   @notifier = nil
 
-  def self.notifier
-    @notifier ||= Slack::Notifier.new(ENV['TIMES_JIO_SLACK_WEBHOOK_URL'], username: 'TECH::CAMP WASEDA SHIFT REMINDER')
-  end
+  class << self
+    def notifier
+      @notifier ||= Slack::Notifier.new(ENV['TIMES_JIO_SLACK_WEBHOOK_URL'], username: 'TECH::CAMP WASEDA SHIFT REMINDER')
+    end
 
-  def self.post(text:)
-    @notifier.post(text: text)
+    def post(text:)
+      notifier.post(text: text)
+    end
   end
-
 end
